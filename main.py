20104a10dd7455fa6fe65b61dff20469abd84396
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2020-03-12 19:49:38 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import csv
+import glob
+
+VERBOSE = False
+PERCENTAGE_CASES = False
+COUNTRIES = ["Mainland China"]
+
+if __name__ == "__main__":
+    population = dict()
+    with open("population.csv", newline='\n') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            population[row[0]] = int(row[1])
+
+    dict_country_cases_time = dict()
+    for file in glob.glob("COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/*.csv"):
+        with open(file, newline='\n') as csvfile:
+            dict_country_cases = dict()
+            dict_country_cases["World"] = (0, 0, 0)
+
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            next(reader)
+            for row in reader:
+                if row[1] in dict_country_cases.keys():
+                    dict_country_cases[row[1]] = (dict_country_cases[row[1]][0] + int(row[3] if row[3] else 0),
+                                                  dict_country_cases[row[1]][1] + int(row[4] if row[4] else 0),
+                                                  dict_country_cases[row[1]][2] + int(row[5] if row[5] else 0))
+                else:
+                    dict_country_cases[row[1]] = (int(row[3] if row[3] else 0),
+                                                  int(row[4] if row[4] else 0),
+                                                  int(row[5] if row[5] else 0))
+                dict_country_cases["World"] = (dict_country_cases["World"][0] + int(row[3] if row[3] else 0),
+                                               dict_country_cases["World"][1] + int(row[4] if row[4] else 0),
+                                               dict_country_cases["World"][2] + int(row[5] if row[5] else 0))
+
+        date = file[-14:-4]
+        dict_country_cases_time[date[6:] + "-" + date[:2] + "-" + date[3:5]] = dict_country_cases
+
+    time_list = list(dict_country_cases_time.keys())
+    time_list.sort()
+
+    confirmed = []
+    deaths = []
+    recovered = []
+
+    for time in time_list:
+        confirmed_ = 0
+        deaths_ = 0
+        recovered_ = 0
+        for country in COUNTRIES:
+            if country in dict_country_cases_time[time]:
+                if VERBOSE:
+                    print(country + " - " + time + ": " + ", ".join(map(str, dict_country_cases_time[time][country])))
+                confirmed_ += dict_country_cases_time[time][country][0]
+                deaths_ += dict_country_cases_time[time][country][1]
+                recovered_ += dict_country_cases_time[time][country][2]
+        if PERCENTAGE_CASES:
+            population_ = 0
+            for country in COUNTRIES:
+                population_ += population[country]
+            confirmed_ = confirmed_ / population_
+            deaths_ = deaths_ / population_
+            recovered_ = recovered_ / population_
+
+        confirmed.append(confirmed_)
+        deaths.append(deaths_)
+        recovered.append(recovered_)
+
+    plt.plot(confirmed)
+    plt.plot(deaths)
+    plt.plot(recovered)
+    plt.ylabel('percentage cases' if PERCENTAGE_CASES else 'total cases')
+    plt.show()
